9059e3853a207913b86675486fc8de92e77354a6
nayarachmi <nayaakusuma@gmail.com>
2024-09-17 18:36:56 +0700

    add input data form

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, create_product_entry
 from main.views import product_list
 from . import views
 
@@ -7,4 +7,5 @@ app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('create-product-entry', create_product_entry, name='create_product_entry'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,12 +1,15 @@
+from django.shortcuts import render, redirect
 from django.shortcuts import render
 from .models import Product # type: ignore
 
 def show_main(request):
+    product_entries = Product.objects.all()
     context = {
         'npm': '2306230685',
         'name': 'Naya Kusumahayati Rachmi',
         'class': 'PBP B',
         'store_name': 'Skivy',
+        'product_entries': product_entries
     }
 
     return render(request, "main.html", context)
@@ -17,3 +20,13 @@ def product_list(request):
         'products': products,
     }
     return render(request, 'main.html', context)
+
+def create_product_entry(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_product_entry.html", context)
