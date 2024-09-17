e53268b636a542f2c06111f83851687956a58d29
nayarachmi <nayaakusuma@gmail.com>
2024-09-17 18:47:50 +0700

    return data in JSON form

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_product_entry, show_xml
+from main.views import show_main, create_product_entry, show_xml, show_json
 from main.views import product_list
 from . import views
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product-entry', create_product_entry, name='create_product_entry'),
     path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
 ]
--- main/views.py
+++ main/views.py
@@ -37,5 +37,8 @@ def show_xml(request):
     data = Product.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 
